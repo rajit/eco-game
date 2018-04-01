7f24b6fcf6b390838ac8eba91accdf129ea91104
Rajit Singh <rajit@qeek.co>
2018-04-01 21:44:58 +0100

    Add sound when picking up rubbish. Always animate robot walking on move

--- Assets/PickupRubbish.cs
+++ Assets/PickupRubbish.cs
@@ -6,20 +6,19 @@ public class PickupRubbish : MonoBehaviour {
 	public GameObject safeZone;
 	public Vector3 position;
 	public Vector3 scale;
+	public AudioClip clip;
 
 	// Use this for initialization
 	void Start () {
-//		print ("console");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 
 	void OnTriggerEnter2D (Collider2D col) {
 		if (col.tag == "Player") {
-			
+			AudioSource.PlayClipAtPoint(clip, transform.position);
 			Instantiate (safeZone, transform.position + position, Quaternion.identity);
 			Destroy (this.gameObject);
 		}
