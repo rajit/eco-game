6dfb5004fb695a4787c65a995c4201bdcb06f9a7
Rajit Singh <rajit@qeek.co>
2018-04-01 15:05:00 +0100

    Constraint monster and player to play area

--- Assets/ConstraintToPlayArea.cs
+++ Assets/ConstraintToPlayArea.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ConstraintToPlayArea : MonoBehaviour {
+	public Renderer target;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () {		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.position = new Vector3 (
+			Mathf.Clamp (transform.position.x, target.bounds.min.x, target.bounds.max.x),
+			Mathf.Clamp (transform.position.y, target.bounds.min.y, target.bounds.max.y),
+			transform.position.z
+		);
 	}
 }
