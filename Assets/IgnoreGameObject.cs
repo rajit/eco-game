9ceedbd616dc1b35beedae2e30e28d4a365b7c47
Rajit Singh <rajit@qeek.co>
2018-04-01 16:00:41 +0100

    Add safezone

---
+++ Assets/IgnoreGameObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreGameObject : MonoBehaviour {
+	public Collider2D objectToIgnore;
+
+	// Use this for initialization
+	void Start () {
+		Physics2D.IgnoreCollision (GetComponent<Collider2D>(), objectToIgnore, true);
+	}
+}
