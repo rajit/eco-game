1e5c3389f7c2b6df2dad97f1108383b98fa2f4ca
Bhavin Padhiar <bhavinpadhiar@Bhavins-MacBook-Pro.local>
2018-04-01 17:42:33 +0100

    Added game over scene

--- Assets/CatchPlayer.cs
+++ Assets/CatchPlayer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CatchPlayer : MonoBehaviour {
 
@@ -15,6 +16,9 @@ public class CatchPlayer : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D (Collider2D col) {
-		print ("Player dies");
+		if (col.tag == "Player") {
+			print ("Player dies");
+			SceneManager.LoadScene ("GameOver");
+		}
 	}
 }
