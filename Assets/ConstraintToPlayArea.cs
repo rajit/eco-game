11a5b0f1d946ec576ac038685ccb7fc2f5c48e03
Bhavin Padhiar <bhavinpadhiar@Bhavins-MacBook-Pro.local>
2018-04-01 16:54:04 +0100

    Fixed boundary and monster rotation

--- Assets/ConstraintToPlayArea.cs
+++ Assets/ConstraintToPlayArea.cs
@@ -12,8 +12,8 @@ public class ConstraintToPlayArea : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.position = new Vector3 (
-			Mathf.Clamp (transform.position.x, target.bounds.min.x, target.bounds.max.x),
-			Mathf.Clamp (transform.position.y, target.bounds.min.y, target.bounds.max.y),
+			Mathf.Clamp (transform.position.x, target.bounds.min.x, target.bounds.max.x - 0.75f),
+			Mathf.Clamp (transform.position.y, target.bounds.min.y, target.bounds.max.y - 1.25f),
 			transform.position.z
 		);
 	}
