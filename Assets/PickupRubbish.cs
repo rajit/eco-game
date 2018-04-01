a7d95bac21f6c367d6c5eb10b074885d1e0a8221
Rajit Singh <rajit@qeek.co>
2018-04-01 14:13:24 +0100

    First commit

---
+++ Assets/PickupRubbish.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupRubbish : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+//		print ("console");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+		print ("OnTriggerEnter " + col);
+		Destroy (this.gameObject);
+	}
+}
